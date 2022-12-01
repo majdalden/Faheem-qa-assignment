bf5a3ba1514d4f0313d6dd28006e0e60358d01a9
majdalden <majd.alden.h@gmail.com>
2022-12-01 13:08:10 +0200

    Solve the assignment

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Driver
 const wdio = require("webdriverio")
+const EC = require('wdio-wait-for');
 
 const opts = {
     path: '/wd/hub',
     port: 4723,
     capabilities: {
         platformName: "Android",
-        platformVersion: "12",
+        platformVersion: "10",
         deviceName: "Android Emulator",
-        app: "<APK_DIRECTORY>/application.apk",
+        app: "/Volumes/MyFiles/MyProjects/MyJavaScriptProjects/qa-assignment/application.apk",
         automationName: "UiAutomator2"
     }
 };
@@ -17,6 +18,10 @@ async function main() {
     const client = await wdio.remote(opts);
     // Write your test code here
 
+    client.waitUntil(EC.elementToBeClickable(client.$("//android.widget.TextView[@text='تصفح كزائر']").click()), { timeout: 5000, timeoutMsg: 'Failed, after waiting for the تصفح كزائر elements' })
+    client.waitUntil(EC.elementToBeClickable(client.$("//android.widget.TextView[@text='ثالث ابتدائي']").click()), { timeout: 5000, timeoutMsg: 'Failed, after waiting for the ثالث ابتدائي elements' })
+
+    // await client.quit();
 }
 
 main();
